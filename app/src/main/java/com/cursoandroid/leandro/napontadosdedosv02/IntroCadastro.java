0946777e534f35e0d538085e54b5aa55171b01a5
Leandro Tofanin <leandrotofanin@hotmail.com>
2018-08-31 21:00:25 -0300

    Primeiro commit

---
+++ app/src/main/java/com/cursoandroid/leandro/napontadosdedosv02/IntroCadastro.java
@@ -0,0 +1,26 @@
+package com.cursoandroid.leandro.napontadosdedosv02;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.cursoandroid.leandro.napontadosdedosv02.activity.CadastroActivity;
+import com.cursoandroid.leandro.napontadosdedosv02.activity.LoginActivity;
+
+public class IntroCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btEntrar(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void btCadastrar(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+}
---
+++ app/src/main/java/com/cursoandroid/leandro/napontadosdedosv02/activity/CadastroActivity.java
@@ -0,0 +1,15 @@
+package com.cursoandroid.leandro.napontadosdedosv02.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cursoandroid.leandro.napontadosdedosv02.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
